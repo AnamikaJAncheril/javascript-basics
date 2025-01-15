c291bad99d34ac47f7b6da3b1dac0c54dee17281
Anamika J Ancheril <anamikajancheril011@gmail.com>
2025-01-15 15:52:07 +0530

    lectures

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ if(num>5){
 
     console.log("number is printed")
 }
+//cannot be a reserve keyword
+//should be meaningful
+//cannot start with a number
+//cannot contain space or hyphen
+//case sensitive
+//
+let firstName = 'Abam' 
+let lastName = 'ani'
+
+let interestRate = 0.3;
+
+let name ='anamika' //string literal
+let age = 19; //number literal
+let isApproved = false; //boolean
+let firstName = undefined;
