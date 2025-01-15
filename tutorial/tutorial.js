770e3dcd22c09d79535707a450e0e491506e04ac
Anamika J Ancheril <anamikajancheril011@gmail.com>
2025-01-15 21:48:10 +0530

    tutorial

---
+++ tutorial/tutorial.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+//string
+
+let x = "Anamika";
+console.log(x);
+console.log(typeof(x));
+
+//number
+
+let y = 123;
+console.log(x);
+console.log(typeof(x));
+
+//number
+
+let z = 1.3;
+console.log(x);
+console.log(typeof(x));
+
