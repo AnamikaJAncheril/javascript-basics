a325ce80cb7a0b06b0fc3bcb02a9bf1accc89a07
Anamika J Ancheril <anamikajancheril011@gmail.com>
2025-01-16 20:38:07 +0530

    tutorial

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -18,3 +18,21 @@ let z = 1.3;
 console.log(x);
 console.log(typeof(x));
 
+let name = 'anamika';
+let age = 19;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
+
+let person = {
+    name: 'anamika'
+}
+console.log(person)
+console.log(typeof(person))
+
+let x = 'Anamika';
+let y = x;
+ 
+x = 'ancheril';
+console.log(x);
+console.log(y);
