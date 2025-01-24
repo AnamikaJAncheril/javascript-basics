ae23a81a1d3738da55090fabe87b5e408e1863ec
Anamika J Ancheril <anamikajancheril011@gmail.com>
2025-01-24 06:42:51 +0530

    todo

--- to-do List App/index.js
+++ to-do List App/index.js
@@ -1,1 +1,12 @@
-alert("welcome to do list application");
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Enter some data");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li); 
+    }
+}
